dd0ffd65cd71661ac060df41dd30896b96ef89ab
teodoravranes <62737677+teodoravranes@users.noreply.github.com>
2020-05-15 19:04:41 +0200

    klasa voce

--- zadatak.cpp
+++ zadatak.cpp
@@ -11,15 +11,79 @@ int cena;
 DinString naziv;
 
 public:
+Proizvod()
+{
+cena = 50;
+naziv = "Nije unet";
+}
+  
 Proizvod(int C, DinString N)
 {
 cena = C;
 naziv = N;
 }
-
-void izracunajPopust() = 0;
-void ispisi() = 0;
+  
+Proizvod(const Proizvod &p)
+{
+cena = p.cena;
+naziv = p.naziv;
 };
 
+virtual void izracunajPopust() = 0;
+virtual void ispisi() = 0;
+};
+
+class Voce : public Proizvod
+{
+private:
+DinString zemljaPorekla;
+int rokTrajanja;
+
+public:
+Voce()
+{
+cena = 50;
+naziv = "Nije unet";
+zemljaPorekla = "Nije uneta";
+rokTrajanja = 2;
+}
+
+Voce(int C, DinString N, DinString ZP, int RT)
+{
+cena = C;
+naziv = N;
+zemljaPorekla = ZP;
+rokTrajanja = RT;
+}
+
+Voce(const &Voce v)
+{
+cena = v.cena;
+naziv = v.naziv;
+zemljaPorekla = v.zemljaPorekla;
+rokTrajanja = v.rokTrajanja;
+}
+
+void izracunajPopust()
+{
+if(rokTrajanja > 5)
+cena =  (67/100)*cena;
+else if(rokTrajanja >= 1 1 && rokTrajanja <= 5)
+cena = (34/100)*cena;
+else if(rokTrajanja==0)
+cena = 0;
+}
+
+void ispisi()
+{
+cout <<"************************************"<<endl;
+cout <<"Cena: "<<cena<<endl;
+cout <<"Naziv: "<<naziv<<endl;
+cout <<"Zemlja porekla: "<<zemljaPorekla<<endl;
+cout <<"Rok trajanja: "<<rokTrajanja<<endl;
+cout <<"************************************"<<endl;
+}
+
+};
 
 
